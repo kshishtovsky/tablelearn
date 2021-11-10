311da3d253d6181f07bda417b47477b3dbe90331
kshishtovsky <mihaylovvs@gmail.com>
2021-11-10 23:59:53 +0500

    1

--- src/components/general/WindowTitle/WindowTitle.jsx
+++ src/components/general/WindowTitle/WindowTitle.jsx
@@ -3,16 +3,16 @@ import './WindowTitle.scss'
 import PropTypes from 'prop-types';
 
 const WindowTitle = (props) => {
-    return (
-        <div className={"card-title--wrapper"}>
-            <p className={"card-title"}>{props.title}</p>
-            <img src={require("../../../assets/icons/arrow-open-card.svg").default} alt=""/>
-        </div>
-    );
+  return (
+    <div className={"card-title--wrapper"}>
+      <p className={"card-title"}>{props.title}</p>
+      <img src={require("../../../assets/icons/arrow-open-card.svg").default} alt=""/>
+    </div>
+  );
 };
 
 WindowTitle.propTypes = {
-    title: PropTypes.string
+  title: PropTypes.string
 };
 
 export default WindowTitle;
